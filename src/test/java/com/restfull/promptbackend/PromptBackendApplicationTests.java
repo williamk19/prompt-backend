acfe1c8d2024d68ce843578480fb0ba99cbfbc4c
williamk19 <williamkurniawan1144@gmail.com>
2023-01-12 10:31:00 +0700

    Initial commit

---
+++ src/test/java/com/restfull/promptbackend/PromptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfull.promptbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
